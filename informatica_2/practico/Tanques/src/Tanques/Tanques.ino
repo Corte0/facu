d460c4ed656655260093cdbe4c9028e93b305d82
corte <cortesiniluciano@gmail.com>
2024-08-22 17:45:53 -0300

    avanza en logica de encender EVs tp tanques

--- informatica_2/practico/Tanques/src/Tanques/Tanques.ino
+++ informatica_2/practico/Tanques/src/Tanques/Tanques.ino
@@ -4,27 +4,39 @@
 Tanque tanques[3] = {
     Tanque(8,9), //pinboya, pin electrovalvula
     Tanque(7,6),
-    Tanque(4,6),
+    Tanque(4,5),
   };
 
 Bomba bomba(13);
 
 void setup() {
   bomba.setUp();
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++)
        tanques[i].setUp();
-  }
+
+  Serial.begin(9600);
+  delay(100);
 }
 
 void loop() {
-  if((tanques[0].isEmpty() || tanques[1].isEmpty() || tanques[2].isEmpty())){
+  int priority = 0;
+
+  if((tanques[0].isEmpty() || tanques[1].isEmpty() || tanques[2].isEmpty()))
     bomba.start();
-    for (int i = 0; i < 3; i++)
-      if (tanques[i].isEmpty()){
-        while(tanques[i].isEmpty())
-          tanques[i].EVOpen();
-        tanques[i].EVClose();
-      }
+  if(!(tanques[0].isEmpty() || tanques[1].isEmpty() || tanques[2].isEmpty()))
     bomba.stop();
+
+  for (int i = 0; i < 3; i++){
+    priority += tanques[i].isEmpty()*(int)pow(2,(3-i));
+    Serial.println(priority);
+    }
+
+
+  for (int i = 0; i < 3; i++){
+    if(priority >= pow(2,3-i) && priority < pow(2,4-i)-1)
+      tanques[i].EVOpen();
+    else{
+      tanques[i].EVClose();
+    }
   }
 }
